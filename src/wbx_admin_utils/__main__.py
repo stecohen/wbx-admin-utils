11c0829b6b5c086f573fec4ffaa6eb23ea110fd0
stcohen <stcohen@cisco.com>
2023-11-24 19:13:48 +0000

    version 19 Added deactivation command debug level 2 is default

--- src/wbx_admin_utils/__main__.py
+++ src/wbx_admin_utils/__main__.py
@@ -7,6 +7,7 @@ import json
 import logging
 import time
 import argparse
+import re
 import urllib
 import numpy            
 
@@ -18,7 +19,7 @@ if ( 'AUTH_BEARER' in os.environ ):
     token = os.environ['AUTH_BEARER']
 
 parser.add_argument("-t", "--token", dest="token", default=token, help="Access token")
-parser.add_argument("-d", "--debug", dest="debug", default=1, help="debug level: 1=errors (default), 2=success, 3=verbose")   
+parser.add_argument("-d", "--debug", dest="debug", default=2, help="debug level: 1=errors, 2=success/info, 3=verbose/debug")   
 # parser.add_argument("-s", "--syntax", action='store_true', default=False, help="command list and syntax")   
 parser.add_argument('command', type=str, help='enter [%(prog)s help command] to list available commands')    
 parser.add_argument('subcommand', type=str, help='')    
@@ -26,13 +27,13 @@ parser.add_argument('parameters', type=str, nargs='*', help='')
 
 args = parser.parse_args()
 
-DEBUG=0
-if (args.debug):
-    DEBUG=int(args.debug)
-    if (DEBUG>=3):
-        logging.getLogger().setLevel(logging.DEBUG)
-        requests_log = logging.getLogger("requests.packages.urllib3")
-        requests_log.setLevel(logging.DEBUG)
+# Traces 
+#
+DEBUG=int(args.debug)
+if (DEBUG>=3):
+    logging.getLogger().setLevel(logging.DEBUG)
+    requests_log = logging.getLogger("requests.packages.urllib3")
+    requests_log.setLevel(logging.DEBUG)
 
 
 #sets the header to be used for authentication and data format to be sent.
@@ -42,7 +43,7 @@ def setHeaders():
     return (spark_header)
 
 def trace(lev, msg):
-    if (DEBUG >= lev):
+    if (DEBUG >= lev ):
         print(msg)
 
 # print items array fields listed in 'il' 
@@ -285,7 +286,7 @@ def user_csv_command(fct, a):
             trace(3,f"in user_csv_command calling fct with {a2}")
             r=fct(a2)
             if (r>0):
-                trace(2,f"{fct.__name__} command successful for user {ue}") 
+                trace(3,f"{fct.__name__} command successful for user {ue}") 
             else:
                 trace(1,f"{fct.__name__} command failed for user {ue}")
             time.sleep(0.01)
@@ -335,6 +336,54 @@ def del_user(a):
     else:
         trace (1, f"del_user: {ue} not found")  
         return(-1)
+    
+###############
+## USERS DEACTIVATION 
+################
+
+# deactivate user from given email 
+# returns 1 if good   
+#
+def deactivate_user(a): 
+    ue=a[0]
+    trace(3,f"in deactivate_user {a}")
+
+    uid=get_user_id(ue)
+    if (uid): 
+        url=f"https://webexapis.com/v1/people/{uid}"
+        r = requests.request("GET", url, headers=setHeaders())
+        s = r.status_code
+        if s == 200 :
+            d=r.json()
+            d['loginEnabled']=False
+            trace(3, f"found user {ue} : sending {d}") 
+            b=json.dumps(d)
+            r = requests.put(url, headers=setHeaders(), data=b, verify=True)
+            s = r.status_code
+            if s in (200,204) :
+                trace(2, f"user {ue} : deactivated")     
+                return(1)
+            else:
+                trace(1,f"deactivate_user : Error {s}")  
+                trace(1,r.text.encode('utf8'))
+                return(-1)
+        else:
+            trace(1,f"deactivate_user : Error {s}")  
+            trace(1,r.text.encode('utf8'))
+            return(-1)
+    else:
+        trace (1, f"deactivate_user: {ue} not found")  
+        return(-1)
+    
+
+# wrapper to decide if passed parameter is single user email or .csv 
+# 
+def uf_deactivate_user(a):
+    m = re.search(".*\.csv$", a[0])
+    if (m):
+        user_csv_command(deactivate_user,a)
+    else:
+        deactivate_user(a)
 
 def uf_del_user(a):
     if (del_user(a) > 0):
@@ -472,6 +521,7 @@ syntax = {
         "list-user-tokens":        {"params":["email"],"fct":uf_list_user_auths, "help":"list user access token"},
         "list-user-tokens-csv":    {"params":["file"],"fct":list_users_auth_in_csv, "help":"list access tokens for users in csv file"},
         "reset-access":          {"params":["email"],"fct":uf_del_user_auths, "help":"reset user access token"},
+        "deactivate":          {"params":["email|csvFile"],"fct":uf_deactivate_user, "help":"de-activate user account. "},
         "reset-access-csv":   {"params":["file"],"fct":reset_users_in_csv, "help":"reset user access token from CSV"},
         "delete-user":           {"params":["email"],"fct":uf_del_user, "help":"delete user "},
         "delete-users-csv":   {"params":["file"],"fct":del_users_in_csv, "help":"delete users listed via email address in given CSV file"},
